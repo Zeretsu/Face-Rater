d606dad2fdbc681f32a6143d092779df65013e1f
Zeretsu <131717260+Zeretsu@users.noreply.github.com>
2025-08-31 22:00:07 +0100

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,40 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+    // App version info
+    getVersion: () => ipcRenderer.invoke('get-app-version'),
+
+    // File operations
+    saveAnalysis: (data) => ipcRenderer.invoke('save-analysis', data),
+    loadAnalysis: () => ipcRenderer.invoke('load-analysis'),
+
+    // Window controls
+    minimizeWindow: () => ipcRenderer.send('minimize-window'),
+    maximizeWindow: () => ipcRenderer.send('maximize-window'),
+    closeWindow: () => ipcRenderer.send('close-window'),
+
+    // Settings
+    getSettings: () => ipcRenderer.invoke('get-settings'),
+    saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+
+    // Export functionality
+    exportResults: (data, format) => ipcRenderer.invoke('export-results', data, format),
+
+    // Platform info
+    getPlatform: () => process.platform,
+
+    // Listen for menu actions
+    onSaveAnalysis: (callback) => {
+        ipcRenderer.on('save-analysis', callback);
+    },
+    onExportResults: (callback) => {
+        ipcRenderer.on('export-results', callback);
+    },
+
+    // Remove listeners
+    removeAllListeners: (channel) => {
+        ipcRenderer.removeAllListeners(channel);
+    }
+});
